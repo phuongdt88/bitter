75f8918a5fc2d49c0c409d97eaaa47f2a2fb68df
Nghia <kidzen@gmail.com>
2015-05-15 18:09:43 +0700

    fix

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsManager : MonoBehaviour {
+	public enum Screens
+	{
+		MainScreen,
+		QuestScreen,
+		SummonScreen,
+		PvpScreen,
+		EnhanceScreen,
+		MenuScreen
+	}
+	public Screens CurrentScreen;
+	public Screens NavigateScreen;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnClick(){
+		if(CurrentScreen!= NavigateScreen)
+			Debug.Log("Current Screen: "+CurrentScreen+"/Next Screen: "+ NavigateScreen);
+		GameObject panel2  = GameObject.Find(NavigateScreen.ToString());
+		NGUITools.SetActive(panel2,true);       
+		GameObject panel1  = GameObject.Find(CurrentScreen.ToString());         
+		NGUITools.SetActive(panel1,false);
+
+	}
+
+}
